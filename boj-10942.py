83aa842310eb3fad507f523d638ec36f8a767427
unagi-zoso <unagi.zoso@gmail.com>
2024-03-03 00:02:46 +0900

    BOJ 10942 파이썬 재귀형식 dp

--- boj-10942.py
+++ boj-10942.py
@@ -1,6 +1,8 @@
-n = int(input())
-nums = [0]+list(map(int, input().split()))
-m = int(input())
+from sys import stdin
+
+n = int(stdin.readline())
+nums = [0]+list(map(int, stdin.readline().split()))
+m = int(stdin.readline())
 
 dp = [[-1 for _ in range(2005)] for _ in range(2005)]
 
@@ -16,5 +18,6 @@ for i in range(n):
             dp[j][j+i] = nums[j] == nums[j+i] and dp[j+1][j+i-1]
     
 for _ in range(m):
-    i, j = map(int, input().split())
+    i, j = map(int, stdin.readline().split())
     print(1 if dp[i][j] else 0)
+    

7dc0d0889444b2620eb025a7266ea5be415b9a2e
unagi-zoso <unagi.zoso@gmail.com>
2024-03-07 19:29:13 +0900

    BOJ 5972 파이썬 일반적 다익스트라

--- boj-5972.py
+++ boj-5972.py
@@ -1,30 +1,23 @@
 from sys import stdin
-from collections import defaultdict, deque
-from heapq import heapify, heappush, heappop
+from heapq import heappush, heappop
 
 n, m = map(int, stdin.readline().split())
 
-edges = defaultdict(lambda: defaultdict(lambda :500000000))
+edges = [[] for _ in range(n+1)]
 
 for _ in range(m):
     f1, f2, cost = map(int, stdin.readline().split())
-    edges[f1][f2] = min(edges[f1][f2], cost)
-    edges[f2][f1] = min(edges[f2][f1], cost)
-
-visited = [False for _ in range(n+1)]
+    edges[f1].append((f2, cost))
+    edges[f2].append((f1, cost))
 
 dist = [500000000 for _ in range(n+1)]
 pq = [(0,1)]
 while pq:
     cur_cost, cur_id = heappop(pq)
-    while visited[cur_id]:
-        cur_cost, cur_id = heappop(pq)
-    dist[cur_id] = cur_cost
-    if cur_id == n:
-        print(dist[n])
-        exit()
-    visited[cur_id] = True
-    for next_id, next_cost in edges[cur_id].items():
-        if visited[next_id]: continue
+    if dist[cur_id] < cur_cost: continue
+    for next_id, next_cost in edges[cur_id]:
+        if dist[next_id] <= cur_cost + next_cost: continue
+        dist[next_id] = cur_cost + next_cost
         heappush(pq, (cur_cost + next_cost,  next_id))
 
+print(dist[n])

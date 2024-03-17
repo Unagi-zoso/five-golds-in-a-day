36c48dd3168a701dd0bf69c660121487c4d3e067
unagi-zoso <unagi.zoso@gmail.com>
2024-03-17 23:54:34 +0900

    BOJ 10653 탑다운

--- boj-10653.py
+++ boj-10653.py
@@ -1,30 +1,24 @@
-import sys
+n, m = map(int, input().split())
+costs = [tuple(map(int, input().split())) for _ in range(n)]
 
-INF = float('inf')
+dist = [[-1 for _ in range(505)] for _ in range(505)]
 
-n, k = map(int, sys.stdin.readline().split())
-point = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
-
-distance = [[0 for _ in range(n)] for _ in range(n)]
 for i in range(n):
     for j in range(n):
-        distance[i][j] = abs(point[i][0] - point[j][0]) + abs(point[i][1] - point[j][1])
+        dist[i+1][j+1] = abs(costs[i][0] - costs[j][0]) + abs(costs[i][1] - costs[j][1])
 
-dp = [[INF for _ in range(n)] for _ in range(k + 1)]
-dp[0][0] = 0
+INF = float('inf')
+dp = [[INF for _ in range(505)] for _ in range(505)]
 
-# k = 0
-for i in range(1, n):
-    dp[0][i] = dp[0][i - 1] + distance[i - 1][i]
+def rec(dest, cntIgnored):
+    global dp, INF, dist
+    if dest == 1:
+        return 0
+    if dp[dest][cntIgnored] != INF:
+        return dp[dest][cntIgnored]
+    for i in range(cntIgnored+1):
+        if dest - 1 - i < 1: break
+        dp[dest][cntIgnored] = min(dp[dest][cntIgnored], rec(dest - 1 - i, cntIgnored - i) + dist[dest - 1 - i][dest])
+    return dp[dest][cntIgnored]
 
-# k = 1, 2, ... k
-for i in range(1, k + 1):
-    dp[i][0], dp[i][1] = 0, dp[i - 1][1]
-    dp[i][i] = distance[0][i]
-    for j in range(1, n):
-        for m in range(i, 0, -1):
-            if j - m - 1 < 0:
-                continue
-            dp[i][j] = min(dp[i][j], dp[i - m][j - m - 1] + distance[j][j - m - 1], dp[i][j - 1] + distance[j - 1][j])
-
-print(dp[-1][-1])
+print(rec(n, m))

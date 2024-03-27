4bb6a7611c533b2a581f6bde93478e9f3ef38c53
unagi-zoso <unagi.zoso@gmail.com>
2024-03-28 00:17:30 +0900

    포도주 n == 1 경우 예외 추가

--- boj-2156.py
+++ boj-2156.py
@@ -5,8 +5,11 @@ for i in range(1, n+1):
 
 dp = [0] * (n+1)
 dp[1] = wine[1]
-dp[2] = wine[1] + wine[2]
-for i in range(2, n+1):
-    dp[i] = max(dp[i-1], dp[i-2] + wine[i], dp[i-3] + wine[i-1] + wine[i])
+if n == 1:
+    print(wine[1])
+    exit()
 
+dp[2] = wine[1] + wine[2]
+for i in range(3, n+1):
+    dp[i] = max([dp[i-1], dp[i-2] + wine[i], dp[i-3] + wine[i-1] + wine[i]])
 print(dp[n])

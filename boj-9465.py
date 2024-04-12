ba307237ec4cfdb0783f1265d3b62c13591509a2
unagi-zoso <unagi.zoso@gmail.com>
2024-04-13 00:04:04 +0900

    정상 버전 업로드

--- boj-9465.py
+++ boj-9465.py
@@ -11,10 +11,8 @@ for _ in range(int(input())):
         dp[0][1] = f_l[1] + s_l[0]
         dp[1][1] = s_l[1] + f_l[0] 
         for i in range(2, n):
-            if i == 2:
-                print(f_l[i] + dp[1][i-1], s_l[i] + dp[0][i-2], s_l[i] + dp[1][i-2])
-            dp[0][i] = max([f_l[i] + dp[1][i-1], s_l[i] + dp[0][i-2], s_l[i] + dp[1][i-2]])
-            dp[1][i] = max([s_l[i] + dp[0][i-1], f_l[i] + dp[0][i-2], f_l[i] + dp[1][i-2]])
+            dp[0][i] = max([f_l[i] + dp[1][i-1], f_l[i] + dp[0][i-2], f_l[i] + dp[1][i-2]])
+            dp[1][i] = max([s_l[i] + dp[0][i-1], s_l[i] + dp[0][i-2], s_l[i] + dp[1][i-2]])
             
         print(max(dp[0][n-1], dp[1][n-1]))
     

36c2dee7f6882e39cdf474fdf720e4e0af7b3de1
Unagi_zoso <unagi.zoso@gmail.com>
2024-10-15 01:26:30 +0900

    programmer 옹알이 1 순열 python

--- prog-babbling.py
+++ prog-babbling.py
@@ -1,3 +1,4 @@
+# 시간복잡도 O(N * |S|)
 def solution(babbling):
     def 유한상태머신(string):
         state = 0
@@ -42,5 +43,24 @@ def solution(babbling):
     answer = 0
     for 옹알옹알 in babbling:
         if 유한상태머신(옹알옹알):
+            answer += 1
+    return answer
+
+
+# 시간복잡도 O(4 * 4! + N)
+from itertools import permutations
+
+def solution(babbling):
+    d = {}
+    for i in range(1, 5):
+        for p in permutations(['aya', 'ye', 'woo', 'ma'], i):
+            s = ""
+            for c in p:
+                s += c
+            d[s] = True
+            
+    answer = 0
+    for 옹알옹알 in babbling:
+        if 옹알옹알 in d:
             answer += 1
     return answer

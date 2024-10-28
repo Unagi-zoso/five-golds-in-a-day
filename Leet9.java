9ae960962909c6e0c567cc715ac2fe4daa9b33c1
Unagi-zoso <Unagi.zoso@gmail.com>
2024-10-29 01:50:57 +0900

    Leet 9 using Integer Java

--- Leet9.java
+++ Leet9.java
@@ -14,3 +14,19 @@ class Solution {
         return true;
     }
 }
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+        int originX = x;
+        int n = 0;
+        while (x > 0) {
+            n *= 10;
+            n += x % 10;
+            x /= 10;
+        }
+        
+        return originX == n;
+    }
+}
+

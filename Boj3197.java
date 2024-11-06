eec31defe451a015d2baf4c1b3eb5e2c33fab64c
Unagi-zoso <Unagi.zoso@gmail.com>
2024-11-06 22:06:08 +0900

    Boj 3197 java

--- Boj3197.java
+++ Boj3197.java
@@ -1,97 +1,94 @@
 import java.util.*;
-import java.io.BufferedReader;
 import java.io.*;
 
 public class Boj3197 {
-    static int[][] dir = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
+    static int[][] dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    static int r, c;
+    static char[][] board;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int r = Integer.parseInt(st.nextToken()), c = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        board = new char[r][c];
 
-        char[][] board = new char[r][c];
+        boolean[][] melted = new boolean[r][c];
+        ArrayDeque<int[]> edgeGlacierQ = new ArrayDeque<>();
+
+        int swanCount = 0;
+        int[][] swans = new int[2][2];
 
         for (int i = 0; i < r; i++) {
             String tS = br.readLine();
             for (int j = 0; j < c; j++) {
                 board[i][j] = tS.charAt(j);
-            }
-        }
-
-        int birdsCount = 0;
-        int[][] birds = new int[2][2];
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
                 if (board[i][j] == 'L') {
-                    birds[birdsCount][0] = i;
-                    birds[birdsCount][1] = j;
-                    birdsCount++;
+                    swans[swanCount][0] = i;
+                    swans[swanCount][1] = j;
+                    swanCount++;
+                }
+                if (board[i][j] == '.' || board[i][j] == 'L') {
+                    edgeGlacierQ.add(new int[]{i, j});
+                    melted[i][j] = true;
                 }
             }
         }
+
+        boolean[][] swanVisited = new boolean[r][c];
+        ArrayDeque<int[]> swanQueue = new ArrayDeque<>();
+        swanQueue.add(swans[0]);
         
         int answer = 0;
         while (true) {
-            boolean flag = bfs(r, c, board, birds[0], birds[1]);
-            if (flag) {
+            if (swanProcess(board, swanQueue, swanVisited, swans[1])) {
                 System.out.println(answer);
                 return;
             }
-            
-            LinkedList<int[]> queue = new LinkedList<>();
-            for (int i = 0; i < r; i++) {
-                for (int j = 0; j < c; j++) {
-                    for (int d = 0; d < 4; d++) {
-                        int edgeY = i + dir[d][0], edgeX = j + dir[d][1];
-                        if (edgeY < 0 || edgeY >= r || edgeX < 0 || edgeX >= c) continue;
-                        if (board[i][j] == 'X' && board[edgeY][edgeX] == '.') {
-                            queue.add(new int[] { i, j });
-                            break;
-                        }
+
+            int size = edgeGlacierQ.size(); // 사이즈를 통해 이전 시간의 원소 다루기 가능. 원래 같음 while (!q.isEmpty()) 로 처리했을 것.
+            for (int i = 0; i < size; i++) {
+                int[] cur = edgeGlacierQ.poll();
+                int y = cur[0], x = cur[1];
+
+                for (int d = 0; d < 4; d++) {
+                    int nY = y + dir[d][0], nX = x + dir[d][1];
+                    if (nY < 0 || nY >= r || nX < 0 || nX >= c) continue;
+                    if (!melted[nY][nX] && board[nY][nX] == 'X') {
+                        board[nY][nX] = '.';
+                        melted[nY][nX] = true;
+                        edgeGlacierQ.add(new int[]{nY, nX});
                     }
                 }
-            }
-
-            while (!queue.isEmpty()) {
-                int[] cur = queue.pollFirst();
-                int y = cur[0], x = cur[1];
-                board[y][x] = '.';
             }
             answer++;
         }
     }
 
-    public static boolean bfs(int r, int c, char[][] board, int[] st, int[] ds) {
-        boolean[][] visited = new boolean[r][c];
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                visited[i][j] = false;
-            }
-        }
-
-        LinkedList<int[]> queue = new LinkedList<>();
-        queue.add(st);
-        visited[st[0]][st[1]] = true;
-
+    public static boolean swanProcess(char[][] board, ArrayDeque<int[]> queue, boolean[][] visited, int[] ds) {
+        ArrayDeque<int[]> nextQueue = new ArrayDeque<>();
         while (!queue.isEmpty()) {
-            int[] cur = queue.pollFirst();
+            int[] cur = queue.poll();
             int y = cur[0], x = cur[1];
 
             for (int d = 0; d < 4; d++) {
                 int nY = y + dir[d][0], nX = x + dir[d][1];
                 if (nY < 0 || nY >= r || nX < 0 || nX >= c) continue;
-                
-                if (visited[nY][nX] || board[nY][nX] == 'X') continue;
-                if (nY == ds[0] && nX == ds[1]) {
-                    
-                    return true;
+
+                if (!visited[nY][nX] && board[nY][nX] != 'X') {
+                    if (nY == ds[0] && nX == ds[1]) {
+                        return true;
+                    }
+                    visited[nY][nX] = true;
+                    queue.add(new int[]{nY, nX});
+                } else if (!visited[nY][nX] && board[nY][nX] == 'X') {
+                    visited[nY][nX] = true;
+                    nextQueue.add(new int[]{nY, nX});
                 }
-                visited[nY][nX] = true;
-                queue.add(new int[] { nY, nX });
             }
         }
-        
+        queue.addAll(nextQueue);
+
         return false;
     }
 }

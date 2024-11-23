581c71d734262d6e7730ae1c3f12da6d5afad158
Unagi-zoso <Unagi.zoso@gmail.com>
2024-11-23 23:56:02 +0900

    Boj 2748 java

---
+++ Boj2748.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Boj2748 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        long[] dp = new long[91];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        bw.write("" + dp[n]);
+        bw.flush();
+    }    
+}
+
+// Extreme Boundary Check

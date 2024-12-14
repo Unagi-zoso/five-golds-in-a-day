ac3c1a412e3cce1885e91547741178b0198eb7be
Unagi-zoso <Unagi.zoso@gmail.com>
2024-12-15 01:12:14 +0900

    도넛과 막대 그래프 ondegree, outdegree 를 통한 정해 java

--- Kakao24WI2.java
+++ Kakao24WI2.java
@@ -2,110 +2,29 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[][] edges) {
+        int MAX = 1000005;
+        int[] indegrees = new int[MAX];
+        int[] outdegrees = new int[MAX];
         
-        int startVertex = -1;
-
-        int[] ods = new int[1000005];
-        int[] ids = new int[1000005];
-        {   
-            for (int[] e : edges) {
-                int od = e[0];
-                int id = e[1];
-                ods[od]++;
-                ids[id]++;
-            }    
-            for (int i = 0; i < 1000005; i++) {
-                if (ods[i] > 1 && ids[i] == 0) startVertex = i;
-            }
-        }
-        
-        int[] unionArr= new int[1000005];
-        {
-            for (int i = 0; i < 1000005; i++) {
-                unionArr[i] = i;
-            }
-        
-            for (int[] e : edges) {
-                int od = e[0];
-                int id = e[1];
-                if (od == startVertex) continue;
-                union(od, id, unionArr);
-            }
-        }
-        
-        class Info {
-            public Set<Integer> set;
-            public Integer cnt;
-            
-            public Info() {
-                set = new HashSet<>();
-                cnt = 0;
-            };
-        }
-        
-        int[] answer = {startVertex, 0, 0, 0};
-        
-        // 각 집합 별 정점의 수와 간선의 수를 저장
-        Map<Integer, Info> m = new HashMap<>();
         for (int[] e : edges) {
-            int od = e[0];
-            int id = e[1];
+            int from = e[0];
+            int to = e[1];
             
-            if (od == startVertex) continue;
-            
-            int root = find(od, unionArr);
-            if (!m.containsKey(root)) {
-                m.put(root, new Info());               
-            } 
-            Info info = m.get(root);
-            info.set.add(od);
-            info.set.add(id);
-            info.cnt++;
+            indegrees[to]++;
+            outdegrees[from]++;
         }
-
-        for (int key : m.keySet()) {
-            Info info = m.get(key);
-            int vCnt = info.set.size();
-            int eCnt = info.cnt;
-            if ((vCnt - eCnt) == 0) {
-                answer[1]++;
-            } else if ((vCnt - eCnt) > 0) {
-                answer[2]++;
-            } else {
-                answer[3]++;
-            }
-        }
-
-        int[] idsNotFromStart = new int[1000005];
-        for (int i = 0; i < 1000005; i++) {
-            idsNotFromStart[i] = -1;
-        }
-        {   
-            for (int[] e : edges) {
-                int od = e[0];
-                int id = e[1];
-                if (idsNotFromStart[id] == -1) idsNotFromStart[id] = 0;
-                if (od == startVertex) continue;
-                idsNotFromStart[id]++;
-            }
-        }
-
-        for (int i = 0; i < 1000005; i++) {
-            if (ods[i] == 0 && idsNotFromStart[i] == 0) answer[2]++;
-        }
-        return answer;
-    }
-    
-    public static int find(int n, int[] unionArr) {
-        if (unionArr[n] == n) return n;
-        return find(unionArr[n], unionArr);
-    }
-    
-    public static void union(int a, int b, int[] unionArr) {
-        int aR = find(a, unionArr);
-        int bR = find(b, unionArr);
         
-        if (aR == bR) return;
-        unionArr[aR] = bR;
+        int origin = -1;
+        int stickCnt = 0;
+        int eightCnt = 0;
+        for (int i = 0; i < MAX; i++) {
+            if (indegrees[i] == 0 && outdegrees[i] >= 2) origin = i;
+            else if (indegrees[i] >= 1 && outdegrees[i] == 0) stickCnt++;
+            else if (indegrees[i] >= 2 && outdegrees[i] == 2) eightCnt++;
+        }
+        int donutCnt = outdegrees[origin] - stickCnt - eightCnt;
+        
+        int[] answer = {origin, donutCnt, stickCnt, eightCnt};
+        return answer;
     }
 }

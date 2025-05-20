9a1c365eac4f56705f0c2fbd5ba9a27f5edcd435
Unagi_zoso <unagi.zoso@gmail.com>
2025-05-21 00:30:22 +0900

    카턴쉽 2024 5

--- 2024kakaowinterinternship5.java
+++ 2024kakaowinterinternship5.java
@@ -1,52 +1,21 @@
 class Solution {
     public int solution(int n, int[] tops) {
-        int MOD = 10007;
         int len = 2 * n + 1;
-
-        int[][] board = new int[2][len];
-        board[1][0] = 1;
-        for (int i = 1; i < len; i++) {
-            if (i % 2 == 1) {
-                board[1][i] = -1;
-                if ((i - 1) / 2 < tops.length && tops[(i - 1) / 2] == 1) {
-                    board[0][i] = 1;
-                } else {
-                    board[0][i] = 0;
-                }
-            } else {
-                board[1][i] = 1;
-                board[0][i] = 0;
-            }
-        }
-
-        int[] dp = new int[len + 1];
+        int[] dp = new int[200_005];
         
-        dp[len] = 1;
-
-        for (int idx = len - 1; idx >= 0; idx--) {
-            int currentWays = 0;
-
-            if (idx + 1 <= len) {
-                currentWays = (currentWays + dp[idx + 1]) % MOD;
-            }
-
-            if (idx < len && board[1][idx] == -1 && board[0][idx] == 1) {
-                if (idx + 1 <= len) {
-                    currentWays = (currentWays + dp[idx + 1]) % MOD;
-                }
-            }
-
-            if (idx + 2 <= len && board[1][idx] == 1 && board[1][idx + 1] == -1) {
-                currentWays = (currentWays + dp[idx + 2]) % MOD;
-            }
-
-            if (idx + 2 <= len && board[1][idx] == -1 && board[1][idx + 1] == 1) {
-                currentWays = (currentWays + dp[idx + 2]) % MOD;
+        dp[0] = 1;
+        dp[1] = 2;
+        if (tops[0] == 1) dp[1]++;
+        
+        for (int i = 2; i < len; i++) {
+            dp[i] = (dp[i] + dp[i - 1]) % 10_007;
+            if (i % 2 == 1 && tops[(i - 1) / 2] == 1) {
+                dp[i] = (dp[i] + dp[i - 1]) % 10_007;
             }
             
-            dp[idx] = currentWays;
+            dp[i] = (dp[i] + dp[i - 2]) % 10_007;
+            
         }
-        
-        return dp[0];
+        return dp[2 * n];
     }
 }

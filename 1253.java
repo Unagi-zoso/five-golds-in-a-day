0446a7c3a0d7e0ee7979bc060588ab7a9f428216
Unagi_zoso <unagi.zoso@gmail.com>
2025-06-04 04:04:35 +0900

    boj 1253 java

--- 1253.java
+++ 1253.java
@@ -7,31 +7,32 @@ class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
-        int[] nums = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] nums = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).sorted().toArray();
 
-        Map<Integer, Integer> cnts = new HashMap<>();
-        for (int num : nums) {
-            cnts.put(num, cnts.getOrDefault(num, 0)+1);
-        }
-        
         int ans = 0;
         for (int i = 0; i < n; i++) {
-            cnts.put(nums[i], cnts.get(nums[i])-1);
-            for (int j = 0; j < n; j++) {
-                if (i == j) continue;
-                cnts.put(nums[j], cnts.get(nums[j])-1);
-                if (cnts.getOrDefault(nums[i] - nums[j], 0) > 0) {
-                    ans++;
-                    cnts.put(nums[j], cnts.get(nums[j])+1);
-                    break;
+            int j = 0;
+            int k = n - 1;
+            while (j < k) {
+                if (j == i) { 
+                    j++;
+                    continue;
                 }
-                cnts.put(nums[j], cnts.get(nums[j])+1);
+                if (k == i) {
+                    k--;
+                    continue;
+                }
+                if (nums[j] + nums[k] == nums[i]) {
+                    ans++;
+                    break;
+                } else if (nums[j] + nums[k] > nums[i]) {
+                    k--;
+                } else if (nums[j] + nums[k] < nums[i]) {
+                    j++;
+                }
             }
-            cnts.put(nums[i], cnts.get(nums[i])+1);
         }
         bw.write(String.valueOf(ans));
         bw.flush();
     }
-
-    
 }

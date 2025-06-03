6e6d16100644ed3a53ab6409653cabaf6a429d05
Unagi_zoso <unagi.zoso@gmail.com>
2025-06-03 16:41:24 +0900

    boj 21939 lazy 접근

--- 21939.java
+++ 21939.java
@@ -22,14 +22,15 @@ class Main {
             }
         });
 
-        Map<Integer, int[]> hashMapForRemove = new HashMap<>();
+        Map<Integer, int[]> map = new HashMap<>();
+        Set<int[]> removed = new HashSet<>();
 
         int n = Integer.parseInt(br.readLine());
         for (int i = 0; i < n; i++) {
             int[] problem = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            hashMapForRemove.put(problem[0], problem);
             mxPq.offer(problem);
             mnPq.offer(problem);
+            map.put(problem[0], problem);
         }
 
 
@@ -41,9 +42,15 @@ class Main {
             if (oper.equals("recommend")) {
                 int type = Integer.parseInt(inputs[1]);
                 if (type == 1) {
+                    while (removed.contains(mxPq.peek())) {
+                        mxPq.poll();  
+                    }
                     int[] target = mxPq.peek();
                     bw.write(String.valueOf(target[0]) + "\n");
                 } else if (type == -1) {
+                    while (removed.contains(mnPq.peek())) {
+                        mnPq.poll();
+                    }
                     int[] target = mnPq.peek();
                     bw.write(String.valueOf(target[0]) + "\n");
                 }
@@ -53,15 +60,18 @@ class Main {
                 int[] problem = { probNum, probLev };
                 mxPq.offer(problem);
                 mnPq.offer(problem);
-                hashMapForRemove.put(problem[0], problem);
+                map.put(problem[0], problem);
             } else if (oper.equals("solved")) {
                 int probNum = Integer.parseInt(inputs[1]);
-                int[] target = hashMapForRemove.get(probNum);
-                mxPq.remove(target);
-                mnPq.remove(target);
-                hashMapForRemove.remove(target);
+                int[] problem = map.get(probNum);
+                map.remove(probNum);
+                removed.add(problem);
             } 
         }
         bw.flush();
     }
 }
+
+/**
+ * lazy 하게 처리하다보니 한 큐에 난이도가 다른 여러 개의 같은 넘버 문제가 있을 수 있다.
+ */

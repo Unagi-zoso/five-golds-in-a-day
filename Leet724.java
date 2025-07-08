71db543f5d61283543ae6af4e52296a218bdf0db
Unagi_zoso <Unagi.zoso@gmail.com>
2025-07-09 00:43:25 +0900

    Leet 724 java

---
+++ Leet724.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int leftSum = 0;
+        int rightSum = 0;
+        for (int n : nums) rightSum += n;
+        for (int i = 0; i < nums.length; i++) {
+            if (i-1 >= 0) leftSum += nums[i-1];
+            rightSum -= nums[i];
+            if (leftSum == rightSum) return i;
+        }
+        return -1;
+    }
+}

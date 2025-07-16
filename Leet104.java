f46d867cef147e5fae153349b8b66be1842a2e39
Unagi_zoso <Unagi.zoso@gmail.com>
2025-07-16 11:16:43 +0900

    Leet 104 java

---
+++ Leet104.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        return rec(root, 0);
+    }
+
+    public int rec(TreeNode node, int depth) {
+        if (node == null) return depth;
+        return Math.max(rec(node.left, depth+1), rec(node.right, depth+1));
+    }
+}

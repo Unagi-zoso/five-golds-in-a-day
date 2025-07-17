92ba80fe67bcef1a0d559311fecc04e3f2799fdd
Unagi_zoso <Unagi.zoso@gmail.com>
2025-07-17 21:41:18 +0900

    Leet 236 java

---
+++ Leet236.java
@@ -0,0 +1,10 @@
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return null;
+        if (root.equals(p) || root.equals(q)) return root;
+        TreeNode l = lowestCommonAncestor(root.left, p, q);
+        TreeNode r = lowestCommonAncestor(root.right, p, q);
+        if (l != null && r != null) return root;
+        return l == null ? r : l;
+    }
+}

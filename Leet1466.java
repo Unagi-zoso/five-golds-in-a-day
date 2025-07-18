0e56f08c7ec8a5486c8f50c95d8cf620f97d991a
Unagi_zoso <Unagi.zoso@gmail.com>
2025-07-18 17:36:57 +0900

    Leet 1466 java

--- Leet1466.java
+++ Leet1466.java
@@ -1,66 +1,28 @@
 class Solution {
+    boolean[] vis = new boolean[100000];
+    List<List<List<Integer>>> con = new ArrayList<>();
     public int minReorder(int n, int[][] connections) {
-        List<List<Integer>> con = new ArrayList<>();
-        for (int[] c : connections) {
-            List<Integer> temp = new ArrayList<>();
-            temp.add(c[1]);
-            temp.add(c[0]);
-            temp.add(0);
-            con.add(temp);
-            temp = new ArrayList<>();
-            temp.add(c[0]);
-            temp.add(c[1]);
-            temp.add(1);
-            con.add(temp);
+        for (int i = 0; i <= n; i++) con.add(new ArrayList<>());
+        for (int[] conn : connections) {
+            List<Integer> temp = Arrays.asList(conn[0], 0);
+            con.get(conn[1]).add(temp);
+            temp = Arrays.asList(conn[1], 1);
+            con.get(conn[0]).add(temp);
         }
-        Collections.sort(con, (a, b)->Integer.compare(a.get(0), b.get(0)));
-        int ans = 0;
-        boolean[] visited = new boolean[100000];
-        LinkedList<Integer> q = new LinkedList<>();
-        q.offer(0);
-        visited[0] = true;
-        while (!q.isEmpty()) {
-            int i = q.poll();
-            int lIdx = bsL(con, i);
-            int rIdx = bsR(con, i);
-            for (int j = lIdx; j < rIdx; j++) {
-                int from = con.get(j).get(0);
-                int to = con.get(j).get(1);
-                int cost = con.get(j).get(2);
-                if (visited[to]) continue;
-                visited[to] = true;
-                q.offer(to);
-                ans += cost;
-            }
-        }
-        return ans;
+
+        vis[0]= true;
+        return rec(0);
     }
 
-    public int bsR(List<List<Integer>> con, int target) {
-        int st = 0;
-        int ed = con.size();
-        while (st < ed) {
-            int mid = st + (ed - st) / 2;
-            if (con.get(mid).get(0) <= target) {
-                st = mid +1;
-            } else if (con.get(mid).get(0) > target) {
-                ed = mid;
-            }
+    public int rec(int to) {
+        int retCost = 0;
+        for (List<Integer> info : con.get(to)) {
+            int from = info.get(0);
+            int cost = info.get(1);
+            if (vis[from]) continue;
+            vis[from] = true;
+            retCost += rec(from) + cost;
         }
-        return st;
-    }
-
-    public int bsL(List<List<Integer>> con, int target) {
-        int st = 0;
-        int ed = con.size();
-        while (st < ed) {
-            int mid = st + (ed - st) / 2;
-            if (con.get(mid).get(0) < target) {
-                st = mid +1;
-            } else if (con.get(mid).get(0) >= target) {
-                ed = mid;
-            }
-        }
-        return st;
+        return retCost;
     }
 }
